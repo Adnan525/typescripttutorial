8ca8af9a5b4925c810f06994942957c85139ceca
adnAdnan525 <adnaan525@gmail.com>
2023-02-05 18:02:20 +0000

    union

--- day2.ts
+++ day2.ts
@@ -1,18 +1,39 @@
-// never type means the code will stop executing
-const myArr: string[] = []
-myArr.push("hello")
-
-type superHero = {
+type SuperHero = {
     name : string,
     power : string,
     weapon : string,
     weakness : string
 }
 
-const superman : superHero = {
+const superman : SuperHero = {
     name : "Superman",
     power : "man of steel",
     weapon : "laser eyes",
     weakness : "kryptonite"
 }
 console.log(superman)
+
+
+// never type means the code will stop executing
+const myArr: string[] = []
+myArr.push("hello")
+// another way to declare an array
+const myArr2: Array<number> = []
+
+// union type
+// not sure what type the variable will be
+let myGrade : number | string = 10
+
+function getDetails(id: number | string): void {
+    if (typeof id === "string") {
+        id.toLowerCase()
+    }
+    else {
+        id += 1
+    }
+}
+
+//array with union type
+let unionArr: (number | string)[] = [1, 2, 3, "hello"]
+let seatAllotment: "aisle" | "window" | "middle"
+seatAllotment = "aisle"
