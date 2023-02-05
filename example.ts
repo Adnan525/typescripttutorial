943339ba199dc32687524bfbfcdb4470787ac9b7
adnAdnan525 <adnaan525@gmail.com>
2023-02-05 17:25:28 +0000

    type tutorial

--- example.ts
+++ example.ts
@@ -7,3 +7,20 @@ function addTwo(num: number): number {
 function getUpper(str: string): string {
   return str.toUpperCase()
 }
+
+type user = {
+  readonly id: number,
+  name: string,
+  age: number,
+  email: string,
+  isActive: boolean
+  creditCard?: string
+} 
+
+let myUser: user = {
+  id: 1,
+  name: "John",
+  age: 25,
+  email: "",
+  isActive: true
+}
